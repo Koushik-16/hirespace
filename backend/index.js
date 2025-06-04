5baf76c1c603bb4477f8aaf1c0e4d8d649309fc5
Koushik-16 <koushikdandapat16@gmail.com>
2025-06-04 18:52:48 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -32,13 +32,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/interview" , sessionRoute);
 
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend' , 'build')));
-  
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname,'../frontend' ,'build', 'index.html'));
-//   })
-// }
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 initializeSocket(server);
 
 
